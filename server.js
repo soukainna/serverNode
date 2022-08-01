7a40d99bf2554222aca5f4be5924c02e9b66324f
soukainna <soukaina1lafdili@gmail.com>
2022-08-01 19:23:00 +0200

    add methode replace

--- server.js
+++ server.js
@@ -7,39 +7,42 @@ let server = http.createServer() //c'est une instance de mon http server
 //je souhaite écouter si il ya une requete et prendra une fonction
 server.on('request', (request, response) => {
     // console.log('il ya eu une requete')
-    // fs.readFile('index.html', (err, data) => 
-    // {
-    //     if(err){
-    //         //throw err
-    //         response.writeHead(404, {
-    //             'content-type' : 'text/html; charset=utf8'
-    //         })
-    //         response.end("Ce fichier n'existe pas")
-    //     } 
+    fs.readFile('index.html', 'utf-8', (err, data) => 
+    {
+        let query = url.parse(request.url, true).query
+        let name = query.name === undefined ? 'anonyme' : query.name
+        if(err){
+            //throw err
+            response.writeHead(404, {
+                'content-type' : 'text/html; charset=utf8'
+            })
+            response.end("Ce fichier n'existe pas")
+        } 
 
-    //     else {
-    //         response.writeHead(200, {
-    //             'content-type': 'text/html; charset=urf8'
-    //         })
-    //         response.write(data)
-    //         response.end()
-    //     }
+        else {
+            response.writeHead(200, {
+                'content-type': 'text/html; charset=urf8'
+            })
+            data = data.replace('{{ name }}', name)
+            response.write(data)
+            response.end()
+        }
 
-    // })
+    })
     // response.writeHead(200, {
     //     'content-type': 'text/html; charset=utf-8'
     // })
     // response.end("salut comment ça va ?")
-    response.writeHead(200, {
-        'content-type': 'text/html; charset=utf8'
-    })
-    console.log(url.parse(request.url, true))
-    let query = url.parse(request.url, true).query
-    let name = query.name
-    console.log(name)
-    // console.log(query)
-    response.write('Bonjour ' + name)
-    response.end()
+    // response.writeHead(200, {
+    //     'content-type': 'text/html; charset=utf8'
+    // })
+    // console.log(url.parse(request.url, true))
+    // let query = url.parse(request.url, true).query
+    // let name = query.name
+    // console.log(name)
+    // // console.log(query)
+    // response.write('Bonjour ' + name)
+    // response.end()
 })
 
 //sur quelle port ecouter
