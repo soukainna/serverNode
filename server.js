5722740b50c8952afad516f7f590b4e867f3bf32
soukainna <soukaina1lafdili@gmail.com>
2022-08-01 11:11:22 +0200

    createServer

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log('salut')
+let http = require('http')
+
+let server = http.createServer() //c'est une instance de mon http server
+
+server.on('request', (request, response) => {
+    console.log('il ya eu une requete')
+})
+
+server.listen(8080)
