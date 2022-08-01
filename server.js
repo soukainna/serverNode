e265abb272da09a3416588878bcbc548c770bedf
soukainna <soukaina1lafdili@gmail.com>
2022-08-01 19:16:41 +0200

    add parseUrl

--- server.js
+++ server.js
@@ -1,34 +1,45 @@
 let http = require('http')
 let fs = require('fs')
+let url = require('url')
 
 let server = http.createServer() //c'est une instance de mon http server
 
 //je souhaite écouter si il ya une requete et prendra une fonction
 server.on('request', (request, response) => {
     // console.log('il ya eu une requete')
-    fs.readFile('index.html', (err, data) => 
-    {
-        if(err){
-            //throw err
-            response.writeHead(404, {
-                'content-type' : 'text/html; charset=utf8'
-            })
-            response.end("Ce fichier n'existe pas")
-        } 
+    // fs.readFile('index.html', (err, data) => 
+    // {
+    //     if(err){
+    //         //throw err
+    //         response.writeHead(404, {
+    //             'content-type' : 'text/html; charset=utf8'
+    //         })
+    //         response.end("Ce fichier n'existe pas")
+    //     } 
 
-        else {
-            response.writeHead(200, {
-                'content-type': 'text/html; charset=urf8'
-            })
-            response.write(data)
-            response.end()
-        }
+    //     else {
+    //         response.writeHead(200, {
+    //             'content-type': 'text/html; charset=urf8'
+    //         })
+    //         response.write(data)
+    //         response.end()
+    //     }
 
-    })
+    // })
     // response.writeHead(200, {
     //     'content-type': 'text/html; charset=utf-8'
     // })
     // response.end("salut comment ça va ?")
+    response.writeHead(200, {
+        'content-type': 'text/html; charset=utf8'
+    })
+    console.log(url.parse(request.url, true))
+    let query = url.parse(request.url, true).query
+    let name = query.name
+    console.log(name)
+    // console.log(query)
+    response.write('Bonjour ' + name)
+    response.end()
 })
 
 //sur quelle port ecouter
