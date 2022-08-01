92201f65e711c46d45538e38395745e871805f14
soukainna <soukaina1lafdili@gmail.com>
2022-08-01 11:30:15 +0200

    add encodage

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ let server = http.createServer() //c'est une instance de mon http server
 //je souhaite écouter si il ya une requete et prendra une fonction
 server.on('request', (request, response) => {
     // console.log('il ya eu une requete')
-    response.writeHead(200)
-    response.end("salut comment ca va ?")
+    response.writeHead(200, {
+        'content-type': 'text/html; charset=utf-8'
+    })
+    response.end("salut comment ça va ?")
 })
 
 //sur quelle port ecouter
