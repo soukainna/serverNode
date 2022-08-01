10ed581abf5a44fa78fb8479f574498657012062
soukainna <soukaina1lafdili@gmail.com>
2022-08-01 11:21:05 +0200

    att end

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ let http = require('http')
 
 let server = http.createServer() //c'est une instance de mon http server
 
+//je souhaite écouter si il ya une requete et prendra une fonction
 server.on('request', (request, response) => {
     // console.log('il ya eu une requete')
     response.writeHead(200)
     response.end("salut comment ca va ?")
 })
 
+//sur quelle port ecouter
 server.listen(8080)
