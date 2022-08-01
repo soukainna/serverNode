f067bf40897ff0710895e5455551ba0ca1349555
soukainna <soukaina1lafdili@gmail.com>
2022-08-01 11:40:28 +0200

    add index.html

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 let http = require('http')
+let fs = require('fs')
 
 let server = http.createServer() //c'est une instance de mon http server
 
 //je souhaite écouter si il ya une requete et prendra une fonction
 server.on('request', (request, response) => {
     // console.log('il ya eu une requete')
-    response.writeHead(200, {
-        'content-type': 'text/html; charset=utf-8'
+    fs.readFile('index.html', (err, data) => 
+    {
+        if(err) throw err
+        response.writeHead(200, {
+            'content-type': 'text/html; charset=urf8'
+        })
+        response.write(data)
+        response.end()
+
     })
-    response.end("salut comment ça va ?")
+    // response.writeHead(200, {
+    //     'content-type': 'text/html; charset=utf-8'
+    // })
+    // response.end("salut comment ça va ?")
 })
 
 //sur quelle port ecouter
