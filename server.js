eff6a8f8cb8246e21207cd87e765273c3b758b04
soukainna <soukaina1lafdili@gmail.com>
2022-08-01 11:49:18 +0200

    add fs to read file

--- server.js
+++ server.js
@@ -6,14 +6,23 @@ let server = http.createServer() //c'est une instance de mon http server
 //je souhaite écouter si il ya une requete et prendra une fonction
 server.on('request', (request, response) => {
     // console.log('il ya eu une requete')
-    fs.readFile('index.html', (err, data) => 
+    fs.readFile('indaex.html', (err, data) => 
     {
-        if(err) throw err
-        response.writeHead(200, {
-            'content-type': 'text/html; charset=urf8'
-        })
-        response.write(data)
-        response.end()
+        if(err){
+            //throw err
+            response.writeHead(404, {
+                'content-type' : 'text/html; charset=utf8'
+            })
+            response.end("Ce fichier n'existe pas")
+        } 
+
+        else {
+            response.writeHead(200, {
+                'content-type': 'text/html; charset=urf8'
+            })
+            response.write(data)
+            response.end()
+        }
 
     })
     // response.writeHead(200, {
