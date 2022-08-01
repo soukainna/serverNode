0cd7ca595fe9128c8cc47cd92805f6fb2f0715eb
soukainna <soukaina1lafdili@gmail.com>
2022-08-01 11:14:28 +0200

    add listen and response

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ let http = require('http')
 let server = http.createServer() //c'est une instance de mon http server
 
 server.on('request', (request, response) => {
-    console.log('il ya eu une requete')
+    // console.log('il ya eu une requete')
+    response.writeHead(200)
+    response.end("salut comment ca va ?")
 })
 
 server.listen(8080)
